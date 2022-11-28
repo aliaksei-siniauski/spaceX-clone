075f00566137c84752c49c8b68ff6deaf044af15
github-username <mail@gmail.com>
2022-11-28 23:48:25 +0100

    feat: add animation to the inner section in page.html

--- src/js/Scroll.js
+++ src/js/Scroll.js
@@ -1,23 +1,33 @@
 export { scrollFunction }
 import { countUp, reset } from "./Counter.js";
 
+const page1IintroContent = document.querySelector('.intro__content');
+const introArrowDown = document.querySelector('.intro__arrow');
+
+
 let scrollStarted = false
+
+
 
 const scrollPage = () => {
     const scrollPosition = window.scrollY;
     if (scrollPosition > 100 && !scrollStarted) {
         countUp()
         scrollStarted = true;
-        console.log(scrollPosition)
+        page1IintroContent.classList.add("hidden")
+        introArrowDown.classList.add("hidden")
+
 
     } else if (scrollPosition < 100 && scrollStarted) {
         reset()
         scrollStarted = false;
+        page1IintroContent.classList.remove("hidden")
+        introArrowDown.classList.remove("hidden")
+
 
     }
 
 }
-
 
 const scrollFunction = () => {
     document.addEventListener('scroll', scrollPage)
